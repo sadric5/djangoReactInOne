659f1f4a57531d8e0f174176bd8f9fa98126b71a
sadric <sadricgamer@gmail.com>
2021-06-22 13:29:03 -0600

    handle the UI with mock data

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,8 @@
 import React from 'react'
 // import ReactDOM, {render} from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaRegThumbsUp, FaRegThumbsDown} from 'react-icons/fa'
+import {BsChat, BsFillChatDotsFill} from 'react-icons/bs'
 
 
 
@@ -10,9 +12,34 @@ const App = (props)=>{
         <div className='container'>
             {
                 props.data.map(data =>(
-                    <div className='card text-center bg-light m-3' key={data.id}>
-                        <h1 className='text-center'>{data.tweet}</h1>
-                    </div>
+                    <div className='card text-center m-3 my-5' key={data.id}>
+
+                        <div className=' card-body bg-light'>
+                            <p className='text-center'>{data.tweet}</p>
+                        </div>
+                        <div className='card-footer text-muted'>
+                            <ul className='nav'>
+                                <li className='nav-item mx-5'>
+                                    <div className='mx-5 text-muted'>
+                                        <button className='btn'><FaRegThumbsUp/></button>
+                                        <b className='mx-2 text-primary'>{data.like}</b>
+                                    </div>
+                                </li>
+                                <li className='nav-item mx-5'>
+                                    <div className='text-muted mx-5'>
+                                        <BsFillChatDotsFill/>
+                                    </div>
+                                </li>
+                                <li className='nav-item mx-5'>
+                                    <div className='mx-5 text-muted'>
+                                    <button className='btn'><FaRegThumbsDown/></button>
+                                        <b className='mx-2 text-primary'>{data.dislike}</b>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+                        </div>
                 ))
             }   
         </div>
