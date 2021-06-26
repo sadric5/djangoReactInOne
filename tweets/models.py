e974ce574ec66d7ebbee39473af74f5b3ccc2d5a
sadric <sadricgamer@gmail.com>
2021-06-26 10:28:34 -0600

    update model to update the like and dislike

--- tweets/models.py
+++ tweets/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+
+def valide_like(value):
+    if value < 0:
+        raise ValidationError('The value shoud be positive integer')
 
 
 class Tweets(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
 
 
 class CommentTweets(models.Model):
@@ -18,17 +27,13 @@ class CommentTweets(models.Model):
         Tweets, related_name='comment', on_delete=models.CASCADE)
     comment = models.TextField(default='No comment')
     comment_date = models.DateTimeField(auto_now_add=True)
-    numberOfLike = models.IntegerField(editable=False, default=0)
-    numberOfDislike = models.IntegerField(editable=False, default=0)
 
-    def save(self, *args, **kwargs):
-        self.numberOfDislike = TweetsLike.objects.filter(like=True).count()
-        self.numberOfDislike = TweetsLike.objects.filter(like=False).count()
-        super().save(*args, **kwargs)
+    def __str__(self):
+        return self.comment
 
 
 class TweetsLike(models.Model):
     tweet = models.ForeignKey(
         Tweets, related_name='like', on_delete=models.CASCADE)
-    like = models.BooleanField(default=False)
-    dislike = models.BooleanField(default=False)
+    likes = models.IntegerField(default=0, validators=[valide_like])
+    dislikes = models.IntegerField(default=0, validators=[valide_like])
