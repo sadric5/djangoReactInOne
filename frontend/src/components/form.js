c738a06a7bf2d9345b330ec414e748535d62db3f
sadric <sadricgamer@gmail.com>
2021-07-13 14:02:16 -0600

    update form.js for new tweet

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -31,13 +31,13 @@ const CreateTweetForm = (props)=>{
             <div className='row'>
 
               <div className='col-10 mr-0 pr-0'>
-                <label htmlFor="exampleFormControlTextarea" className='container-fluid mr-0'>
+                <label htmlFor="exampleFormControlTextarea" className='container-fluid m-0 p-0'>
                   <textarea className="form-control mystyle textarea" id="exampleFormControlTextarea" rows="6" onChange={(e)=>props.onChange.handleChange(e)}></textarea>
                 </label>
               </div>
-
-              <div className=' ml-0 col-2 pl-0 pt-5'>
-                <button className='m-0 col-2 subm' type='submit' name='csrftoken' className=''><IoMdSend/></button>
+ 
+              <div className=' ml-0 col-2 pl-0 pt-5 mybnt'>
+                <button className='m-0 col-2 subm p-0' type='submit' name='csrftoken'><IoMdSend style={{color:"#ADC8FF"}} size={60} className='mx-0 px-0'/></button>
               </div>
 
             </div>
