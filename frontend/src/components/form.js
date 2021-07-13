5d3a42b5130a84083b0cc79363a4020044b35fea
sadric <sadricgamer@gmail.com>
2021-07-13 12:45:35 -0600

    new tweet form settup

--- frontend/src/components/form.js
+++ frontend/src/components/form.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
+import ReactDOM from 'react-dom';
 import { IoMdSend } from "react-icons/io";
+import './form.css'
 
 function getCookie(name) {
   let cookieValue = null;
@@ -23,13 +25,23 @@ const csrftoken = getCookie('csrftoken');
 
 const CreateTweetForm = (props)=>{
     return (
-        <form>
+        <form className='myform'>
           <div className="mb-3">
-            <label htmlFor="exampleFormControlTextarea" className="form-label">Your tweet</label>
-            <textarea className="form-control" id="exampleFormControlTextarea" rows="3" onChange={(e)=>props.onChange.handleChange(e)}></textarea>
-          </div>
-          <div className="mb-3">
-            <button type='submit' name='csrftoken' className='m-2'><IoMdSend/></button>
+
+            <div className='row'>
+
+              <div className='col-10 mr-0 pr-0'>
+                <label htmlFor="exampleFormControlTextarea" className='container-fluid mr-0'>
+                  <textarea className="form-control mystyle textarea" id="exampleFormControlTextarea" rows="6" onChange={(e)=>props.onChange.handleChange(e)}></textarea>
+                </label>
+              </div>
+
+              <div className=' ml-0 col-2 pl-0 pt-5'>
+                <button className='m-0 col-2 subm' type='submit' name='csrftoken' className=''><IoMdSend/></button>
+              </div>
+
+            </div>
+
           </div>
         </form>
     )
--- frontend/src/components/heaaderButton.js
+++ frontend/src/components/heaaderButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NewTweetButton = (props)=>{
     return (
         <div>
-            <button className='text-primary ms-0' onClick={()=>props.onClick.handleNewTweetControleur()}>New Tweet</button>
+            <button className='text-primary ms-0 btn bg-secondary my-3 rounded ' onClick={()=>props.onClick.handleNewTweetControleur()}>New Tweet</button>
         </div>
     )
 }
