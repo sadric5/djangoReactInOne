53fdf9e6557fb5d6bfd9b139603c2d17af7a3ec2
sadric <sadricgamer@gmail.com>
2021-07-19 13:22:31 -0600

    setting up the login page to request login before write a new tweet

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', index, name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
